effbfd4c8aa641fc06e093b53c4d389d147947d9
juanchis1020 <159039512+juanchis1020@users.noreply.github.com>
2025-10-22 15:56:02 -0500

    Add files via upload

---
+++ src/models/articuloModels.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+const animalSchema = mongoose.Schema({
+  titulo: {
+    type: String,
+    required: true,
+  },
+  autores: {
+    type: String,
+    required: true,
+  },
+  año_de_publicación: {
+    type: String,
+    required: true,
+  },
+  fecha: {
+    type: Date,
+    requiered: true,
+  },
+  resumen: {
+    type: String,
+    required: true,
+  },
+  cantidad_referencias: {
+    type: Number,
+    required: true,
+  },
+  nombre_bases_de_datos_consultada: {
+    type: String,
+    required: true,
+  },
+  nombre_revista: {
+    type: String,
+    required: true,
+  },
+  enlance: {
+    type: String,
+    required: true,
+  }
+});
+module.exports = mongoose.model("Animal", animalSchema);
---
+++ src/routes/articuloRoutes.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const articulolSchema = require("../models/articulolModels");
+//crear articulo
+router.post("/articulos", (req, res) => {
+    const articulo = articuloSchema(req.body);
+    articulo
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+// Consultar todos los artículos
+router.get("/articulos", (req, res) => {
+  articuloSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Consultar un artículo por su id
+router.get("/articulos/:id", (req, res) => {
+  const { id } = req.params;
+  articuloSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+// Modificar un artículo por su id
+router.put("/articulos/:id", (req, res) => {
+  const { id } = req.params;
+  const { 
+    titulo, autores, año_de_publicación, resumen, 
+    cantidad_referencias, nombre_bases_de_datos_consultada, nombre_revista, enlace
+  } = req.body;
+
+  articuloSchema
+    .updateOne(
+      { _id: id },
+      { 
+        $set: { 
+          titulo, autores, año_de_publicación, resumen, 
+          cantidad_referencias, nombre_base_de_datos, nombre_revista, enlace, 
+        } 
+      }
+    )
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Eliminar un artículo por su id
+router.delete("/articulos/:id", (req, res) => {
+  const { id } = req.params;
+  articuloSchema
+    .findByIdAndDelete(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+module.exports = router;
